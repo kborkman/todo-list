81c34d3c6e85b4746b1a8de9cbb8a34c83282a0e
Kevin Borkman <kevin.borkman@hgv.com>
2023-08-14 14:46:30 -0400

    refactored code and added comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,219 +1,214 @@
-let taskCount = -1;
-let compTitle = document.getElementById('completed-title'); 
-let compCount = [];
-let compCheck;
-let container = document.getElementById('tasks-create');
-let tasks =  [];
-let completedTasks = [];
-let editArr = []
-let tarTitle = '';
-let tarDesc = '';
-let tarId = '';
+let tasksCreateContainer = document.getElementById("tasks-create");
+let tasksCompleteContainer = document.getElementById("tasks-complete");
+let closeIcon = document.querySelector(".prompt-close");
+let completedTitle = document.getElementById("completed-title");
+let completedCounter = document.getElementById("completed-number");
+let completedPlural = document.getElementById("completed-plural");
+let prompt = document.getElementById("prompt");
+let addTaskIcon = document.getElementById("prompt-click");
+let updateBtn = document.getElementById("prompt-submit");
+let inputName = document.getElementById("input-name");
+let inputDesc = document.getElementById("input-desc");
+let tasks = [];
+let editTaskObj = null;
 
-const tasksCreate = 'tasks-create';
-const tasksComplete = 'tasks-complete';
+//global event listeners (elements that exist when page loads)
+tasksCreateContainer.addEventListener("dragover", onDragOver);
+closeIcon.addEventListener("click", togglePrompt);
+addTaskIcon.addEventListener("click", addTaskBtnClicked);
+updateBtn.addEventListener("click", promptSubmit);
 
-function updateTasks(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let taskDiv = document.getElementById(tasksCreate);
-        let div = document.createElement('a');
-        // let checked = taskState === 'tasks-complete' ? 'checked' : 'unchecked';
-        if (!arr[i].checked) {
-            taskDiv = document.getElementById(tasksCreate);
-            if (!document.getElementById(arr[i].id)) {
-                div.id = arr[i].id;
-                div.classList.add('draggable');
-                div.draggable = true;
-                div.innerHTML = `
-            <div href="#" class='tasks-wrap border-glow'>
-                <div class="tasks-left">
-                    <h4>${ arr[i].name }</h4>
-                    <p>${ arr[i].desc }</p>
-                </div>
-                <div id="check-wrap" class="tasks-right">
-                    <div class="prompt-delete"><i class="fa fa-trash"></i></div>
-                    <input type="checkbox" id="${ arr[i].id }" class="mark-complete">
-                </div>
+// Create Tasks Dom 
+function updateTasks() {
+  tasksCreateContainer.innerHTML = "";
+  tasksCompleteContainer.innerHTML = "";
+
+  tasks.forEach((task) => {
+    // no need to do anything if the task already exists
+    // 'return' makes `forEach()` skip to next iteration.
+    // Early returns help make code more efficient.
+    if (document.getElementById(task.id)) return;
+
+    let tasksContainer;
+    let taskEl = document.createElement("div");
+
+    if (task.checked) {
+      tasksContainer = tasksCompleteContainer;
+    } else {
+      tasksContainer = tasksCreateContainer;
+    }
+
+    taskEl.id = task.id;
+    taskEl.classList.add("draggable");
+    taskEl.draggable = true;
+    taskEl.innerHTML = `
+        <div class='tasks-wrap border-glow ${task.checked ? "checked" : ""}'>
+            <div class="tasks-left">
+                <h4>${task.name}</h4>
+                <p>${task.desc}</p>
             </div>
-            `;
-            taskDiv.appendChild(div);
-            }
-        } else {
-            taskDiv = document.getElementById(tasksComplete);
-            if (!document.getElementById(arr[i].id)) {
-                div.id = arr[i].id;
-                div.innerHTML = `
-            <div class='tasks-wrap border-glow checked'>
-                <div class="tasks-left">
-                    <h4>${ arr[i].name }</h4>
-                    <p>${ arr[i].desc }</p>
-                </div>
-                <div id="check-wrap" class="tasks-right">
-                    <div class="prompt-delete"><i class="fa fa-trash"></i></div>
-                    <input type="checkbox" id="${ arr[i].id }" class="mark-complete" checked>
-                </div>
+            <div id="check-wrap" class="tasks-right">
+                <div class="prompt-delete"><i class="fa fa-trash"></i></div>
+                <input type="checkbox" id="${task.id}" class="mark-complete" ${
+      task.checked ? "checked" : ""
+    }>
             </div>
-            `;
-            taskDiv.appendChild(div);
-            }
-        }
-    } 
-    if (compCount.length > 0) {
-        compTitle.classList.remove('hidden');
-    } else if (compCount.length === 0 && !compTitle.classList.contains('hidden')) {
-        compTitle.classList.add('hidden');
-    } 
-};
+        </div>`
+
+    //apply listeners to taskEl tag here
+    taskEl.addEventListener("dragstart", onDrag);
+    taskEl.addEventListener("dragend", onDrag);
+    tasksContainer.appendChild(taskEl);
+
+    // all other elements above had to be added to DOM first before we can apply listeners
+    taskEl.querySelector(".tasks-left").addEventListener("click", updateTask);
+    taskEl.querySelector(".fa-trash").addEventListener("click", deleteTask);
+    taskEl
+      .querySelector(".mark-complete")
+      .addEventListener("change", toggleIsComplete);
+  });
+  updateCompletedUI();
+}
+
+function updateCompletedUI() {
+  // filter returns all array elements that match conditional
+  let completedTasksCount = tasks.filter((task) => task.checked).length;
+  completedCounter.innerText = completedTasksCount;
+
+  //handle title
+  if (completedTasksCount) {
+    completedTitle.classList.remove("hidden");
+  } else {
+    completedTitle.classList.add("hidden");
+  }
+
+  //handle plural
+  if (completedTasksCount > 1) {
+    completedPlural.classList.remove("hidden");
+  } else {
+    completedPlural.classList.add("hidden");
+  }
+}
+
+// made a single function to handle all drag events
+function onDrag(e) {
+  switch (e.type) {
+    case "dragstart":
+      e.target.classList.add("dragging");
+      break;
+    case "dragend":
+      e.target.classList.remove("dragging");
+      break;
+  }
+}
+
+function updateTask(e) {
+  let taskId = e.target.closest(".draggable").id;
+  // 'find' method returns first array item satisfying conditional
+  let task = tasks.find((task) => task.id == taskId);
+  editTaskObj = {
+    id: task.id,
+    name: task.name,
+    desc: task.desc
+  };
+  inputName.value = task.name;
+  inputDesc.value = task.desc;
+  togglePrompt();
+}
 
 function taskCheck(id) {
-    for (let k = 0; k < tasks.length; k++) {
-        if (tasks[k].id == id) {
-            tasks[k].checked = !tasks[k].checked;
-            if (compCount.length > 0) {
-                compCheck = tasks[k].checked ? compCount.push(tasks[k]) : compCount.pop();
-            } else if (compCount.length === 0 && tasks[k].checked) {
-                compCheck = compCount.push(tasks[k]);
-            }
-        }
-    }
-    document.getElementById('completed-number').innerText = `${compCount.length} `;
-    let plural = document.getElementById('completed-plural');
-    compCount.length > 1 ? plural.classList.remove('hidden') : plural.classList.add('hidden');
-    redrawTasks();
+  tasks.forEach((task) => {
+    if (task.id != id) return;
+    task.checked = !task.checked;
+  });
+  updateTasks();
 }
 
-function deleteTask(id) {
-    console.log(id);
-    for (let p = 0; p < tasks.length; p++) {
-        if (tasks[p].id == id) {
-            if (tasks[p].checked) {
-                compCount.pop();
-                console.log(compCount);
-                document.getElementById('completed-number').innerText = `${compCount.length} `;
-            }
-            tasks.splice(p, 1);
-        }
-    }
-    redrawTasks();
+function deleteTask(e) {
+  let taskId = parseInt(e.target.closest(".draggable").id);
+  let taskIdx = tasks.findIndex((task) => task.id == taskId);
+  tasks.splice(taskIdx, 1);
+  updateTasks();
 }
 
-function redrawTasks() {
-    document.querySelector("#tasks-create").innerHTML = " ";
-    document.querySelector("#tasks-complete").innerHTML = " ";
-    updateTasks(tasks);
+function addTaskBtnClicked() {
+  inputName.value = "";
+  inputDesc.value = "";
+  togglePrompt();
 }
 
 function togglePrompt() {
-    document.getElementById('prompt').classList.toggle('hidden');
-    document.getElementById('prompt').classList.toggle('blur');
+  if (prompt.open) {
+    prompt.close();
+  } else {
+    prompt.show();
+  }
 }
 
 function editTasks(id) {
-    for (let l = 0; l < tasks.length; l++) {
-        if (tasks[l].id == id) {
-            tasks[l].name = editArr[0].name;
-            tasks[l].desc = editArr[0].desc;
-        }
-    }
-    redrawTasks();
-    editArr.splice(0, editArr.length);
+  tasks.forEach((task) => {
+    // skip to next iteration if ids don't match
+    if (task.id != id) return;
+
+    task.name = editTaskObj.name;
+    task.desc = editTaskObj.desc;
+  });
+  editTaskObj = null;
+  updateTasks();
 }
 
-function clearInput(target) {
-    if (target.value) {
-        target.value = '';
-    }
+function toggleIsComplete(e) {
+  e.target.closest(".tasks-wrap").classList.toggle("checked");
+  e.target.closest(".tasks-wrap").classList.add(e.target.id);
+  taskCheck(e.target.id);
 }
 
-document.getElementById('prompt-click').addEventListener('click', ()=> {
-    togglePrompt();
-});
+function promptSubmit(e) {
+  e.preventDefault();
+  let nameVal = document.getElementById("input-name").value;
+  let descVal = document.getElementById("input-desc").value;
+  if (editTaskObj) {
+    editTaskObj.name = nameVal;
+    editTaskObj.desc = descVal;
+    editTasks(editTaskObj.id);
+  } else {
+    tasks.push({
+      id: tasks.length + 1,
+      name: nameVal,
+      desc: descVal,
+      checked: false
+    });
+  }
+  updateTasks();
+  togglePrompt();
+}
 
-document.getElementById('prompt-submit').addEventListener('click', (e) => {
-    let nameVal = document.getElementById('input-name').value;
-    let descVal = document.getElementById('input-desc').value;
-    e.preventDefault();
-    if (editArr.length > 0) {
-        editArr[0].name = nameVal;
-        editArr[0].desc = descVal;
-        console.log(editArr);
-        editTasks(editArr[0].id)
-    } else {
-        let newObj = {
-            id: taskCount += 1,
-            name: nameVal,
-            desc: descVal,
-            checked: false
-        };
-        tasks.push(newObj);
-    }
-    updateTasks(tasks);
-    togglePrompt();
-});
-
-document.addEventListener('click', (e)=> {
-    const target = e.target;
-    if (target.classList.contains('tasks-wrap')) {
-        console.log(target);
-        tarTitle = e.target.querySelector('h4').innerText;
-        tarDesc = e.target.querySelector('p').innerText;
-        tarId = e.target.parentNode.id;
-        let tempObj = {
-            id: tarId,
-            name: tarTitle,
-            desc: tarDesc,
-        };
-        editArr.push(tempObj);
-        togglePrompt();
-    }
-    if (target.classList.contains('fa-trash')) {
-        deleteTask(target.parentElement.nextElementSibling.id);
-    }
-    if (target.classList.contains('mark-complete')) {
-       target.closest('.tasks-wrap').classList.toggle('checked');
-       target.closest('.tasks-wrap').classList.add(`${e.target.id}`);
-       taskCheck(e.target.id);
-    }
-    if (target.classList.contains('prompt-close')) {
-        togglePrompt();
-    }
-});
-
-document.addEventListener('dragstart', e => {
-    if (e.target.classList.contains('draggable')) {
-       e.target.classList.add('dragging');
-    }
-});
-
-document.addEventListener('dragend', e => {
-    if (e.target.classList.contains('draggable')) {
-       e.target.classList.remove('dragging');
-    }
-});
-
-
-container.addEventListener('dragover', e => {
-    e.preventDefault();
-    const afterElement = getDragElement(e.clientY);
-    const draggable = document.querySelector('.dragging');
-    if (afterElement == null) {
-        container.appendChild(draggable);
-    } else {
-        console.log(container);
-        container.insertBefore(draggable, afterElement);
-    }
-});
+function onDragOver(e) {
+  e.preventDefault();
+  const afterElement = getDragElement(e.clientY);
+  const draggable = document.querySelector(".dragging");
+  if (!afterElement) {
+    tasksCreateContainer.appendChild(draggable);
+  } else {
+    tasksCreateContainer.insertBefore(draggable, afterElement);
+  }
+}
 
 function getDragElement(y) {
-    const draggableElems = [...document.querySelectorAll('.draggable:not(.dragging)')];
+  const draggableElems = [
+    ...document.querySelectorAll(".draggable:not(.dragging)")
+  ];
 
-    return draggableElems.reduce((closest, child) => {
-        const box = child.getBoundingClientRect();
-        const offset = y - box.top - box.height / 2;
-        if (offset < 0 && offset > closest.offset) {
-            return { offset: offset, element: child };
-        } else {
-            return closest;
-        }
-    }, { offset: Number.NEGATIVE_INFINITY }).element;
+  return draggableElems.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
